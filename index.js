829b6a644cd927128725f81b69b97751ad833135
Nguyen Duong <muoi07052001@gmail.com>
2023-03-26 11:17:04 +0700

    Add create and get categories

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
 
 dotenv.config();
 app.use(express.json());
@@ -17,7 +18,7 @@ mongoose
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
-app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
 
 app.listen('4000', () => {
   console.log('Backend is running');
