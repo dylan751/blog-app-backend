3e1483d39a491248596c435305afd10212988b17
Nguyen Duong <muoi07052001@gmail.com>
2023-03-26 10:45:53 +0700

    Add Create post

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 app.use(express.json());
@@ -16,6 +17,7 @@ mongoose
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 
 app.listen('4000', () => {
   console.log('Backend is running');
